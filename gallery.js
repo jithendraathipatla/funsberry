46c1688e60fdc0b84fd295f0877e6e1724b0885e
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-12-15 16:39:50 +0530

    second commit

--- gallery.js
+++ gallery.js
@@ -9,16 +9,7 @@ function closeModal() {
   // document.getElementById("fixingthenav").style.display = "block";
 }
 
-function openModal1() {
-  document.getElementById("myModal1").style.display = "block";
- 
-}
-
-function closeModal1() {
-  document.getElementById("myModal1").style.display = "none";
-}
-
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -50,106 +41,45 @@ function showSlides(n) {
 }
 
 
+
 function floarplanH(){
-  document.getElementById("myModal11").style.display = "block";
-  console.log("hi");
+  document.getElementById("myModalFloor").style.display = "block";
 }
 
-
-function floarplanH1(){
-  document.getElementById("myModal12").style.display = "block";
-  console.log("hi");
+function closeModalabc(){
+  document.getElementById("myModalFloor").style.display = "none";
 }
 
+let slideIndexFloorabc = 1;
+showSlidesFloor(slideIndexFloorabc);
 
-function floarplanH2(){
-  document.getElementById("myModal13").style.display = "block";
-  
+// Next/previous controls
+function plusSlidesFloor(n) {
+  showSlidesFloor(slideIndexFloorabc += n);
 }
 
-function closeModal1(){
-  document.getElementById("myModal11").style.display = "none";
+// Thumbnail image controls
+function currentFloor(n) {
+  showSlidesFloor(slideIndexFloorabc = n);
 }
 
-function closeModal12(){
-  document.getElementById("myModal12").style.display = "none";
+function showSlidesFloor(n) {
+  let j;
+  let slidesFloor = document.getElementsByClassName("mySlidesFloor");
+  var dotsabc = document.getElementsByClassName("demoFloor");
+  if (n > slidesFloor.length) {slideIndexFloorabc = 1}
+  if (n < 1) {slideIndexFloorabc = slidesFloor.length}
+  for (j = 0; j < slidesFloor.length; j++) {
+    slidesFloor[j].style.display = "none";
+  }
+  for(j = 0; j < dotsabc.length; j++) {
+    dotsabc[j].className = dotsabc[j].className.replace(" active", "");
+  }
+  slidesFloor[slideIndexFloorabc - 1].style.display = "block";
+  dotsabc[slideIndexFloorabc - 1].className += " active";
 }
-
-function closeModal13(){
-  document.getElementById("myModal13").style.display = "none";
-}
-
-function floarplanE(){
-  document.getElementById("myModalE").style.display = "block";
-}
-
-function floarplanE1(){
-  document.getElementById("myModalE1").style.display = "block";
-
-}
-
-function floarplanE2(){
-  document.getElementById("myModalE2").style.display = "block";
-}
-
-function floarplanE3(){
-  document.getElementById("myModalE3").style.display = "block";
-}
-
-function closeModalE(){
-  document.getElementById("myModalE").style.display = "none";
-}
-
-function closeModalE1(){
-  document.getElementById("myModalE1").style.display = "none";
-}
-
-function closeModalE2(){
-  document.getElementById("myModalE2").style.display = "none";
-}
-
-function closeModalE3(){
-  document.getElementById("myModalE3").style.display = "none";
-}
-
-
-
-function floarplanS(){
-  document.getElementById("myModalS").style.display = "block";
-}
-
-function floarplanS1(){
-  document.getElementById("myModalS1").style.display = "block";
-
-}
-
-function floarplanS2(){
-  document.getElementById("myModalS2").style.display = "block";
-}
-
-function floarplanS3(){
-  document.getElementById("myModalS3").style.display = "block";
-}
-
-function closeModalS(){
-  document.getElementById("myModalS").style.display = "none";
-}
-
-function closeModalS1(){
-  document.getElementById("myModalS1").style.display = "none";
-}
-
-function closeModalS2(){
-  document.getElementById("myModalS2").style.display = "none";
-}
-
-function closeModalS3(){
-  document.getElementById("myModalS3").style.display = "none";
-}
-
 
 let win = $(window).width();
-
 if(win < 420){
   $("#bc").click(function(){
     $("html, body" ).animate({scrollTop:1920}, 'slow');
