38e8b7b34797a4939fad4bc7d23858b50a45068c
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-12-23 16:35:30 +0530

    second change

--- gallery.js
+++ gallery.js
@@ -132,6 +132,30 @@ function closeModalMasterplan1(){
   document.querySelector("#myModalMasterplan1").style.display = "none";
 }
 
+
+$(document).ready(function(){
+  $('.customer-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
+  });
+});
+
 let win = $(window).width();
 if(win < 420){
   $("#bc").click(function(){
